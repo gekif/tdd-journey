58ff343286bf436a1841c65ab8e815e05bff18cd
Gekif <dzulfikar.maulana@gmail.com>
2020-02-09 10:35:08 +0700

    TDD On Auth Feature

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
     public function authenticate(Request $request){
-        //Validate fields
-        $this->validate($request,['email' => 'required|email','password'=> 'required']);
+        // Validate fields
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password'=> 'required'
+        ]);
 
-        //Attempt validation
-        $credentials = $request->only(['email','password']);
+        // Attempt validation
+        $credentials = $request->only([
+            'email','password'
+        ]);
 
-        if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Incorrect credentials'], 401);
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json([
+                'error' => 'Incorrect credentials'
+            ], 401);
         }
 
         return response()->json(compact('token'));
@@ -23,7 +33,7 @@ class AuthController extends Controller
 
 
     public function register(Request $request){
-        //Validate fields
+        // Validate fields
         $this->validate($request,[
             'email' => 'required|email|max:255|unique:users',
             'name' => 'required|max:255',
